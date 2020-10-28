1dac598d86944b5ec6b545682def4ba1e96ac7f7
FabianoAndradeMaxnet <fabianoandrade@terra.com.br>
2020-10-28 15:35:40 -0300

    Ajustes no EndPoint /categorias/{id}

--- src/main/java/com/maxnet/cursomc/services/CategoriaService.java
+++ src/main/java/com/maxnet/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.maxnet.cursomc.domain.Categoria;
 import com.maxnet.cursomc.repositories.CategoriaRepository;
+import com.maxnet.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(),null));
 	}
 }
