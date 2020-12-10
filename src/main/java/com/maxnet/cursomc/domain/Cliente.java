22d0a53a45ca7c1722f56ad1d9fae7b642591ba3
FabianoAndradeMaxnet <fabianoandrade@terra.com.br>
2020-12-09 21:48:43 -0300

    EndPoint /pedido/{id} disponibilizado

--- src/main/java/com/maxnet/cursomc/domain/Cliente.java
+++ src/main/java/com/maxnet/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.maxnet.cursomc.domain.enums.TipoCliente;
 
@@ -27,6 +28,8 @@ public class Cliente implements Serializable{
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@JsonBackReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
---
+++ src/main/java/com/maxnet/cursomc/services/PedidoService.java
@@ -0,0 +1,21 @@
+package com.maxnet.cursomc.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.maxnet.cursomc.domain.Pedido;
+import com.maxnet.cursomc.repositories.PedidoRepository;
+import com.maxnet.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(),null));
+	}
+}
