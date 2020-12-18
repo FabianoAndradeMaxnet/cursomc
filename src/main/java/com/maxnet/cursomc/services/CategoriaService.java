ba48c5a73e277c747a948e39c429a6ca6338fc11
FabianoAndradeMaxnet <fabianoandrade@terra.com.br>
2020-12-18 09:18:15 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/maxnet/cursomc/services/CategoriaService.java
+++ src/main/java/com/maxnet/cursomc/services/CategoriaService.java
@@ -18,4 +18,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(),null));
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
