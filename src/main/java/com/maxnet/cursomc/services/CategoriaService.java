791402d3bf92b85ff0ff5a889ad2d3dc1ad65493
FabianoAndradeMaxnet <fabianoandrade@terra.com.br>
2020-12-21 21:13:14 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/maxnet/cursomc/services/CategoriaService.java
+++ src/main/java/com/maxnet/cursomc/services/CategoriaService.java
@@ -1,10 +1,14 @@
 package com.maxnet.cursomc.services;
 
 import java.util.Optional;
+
+import org.hibernate.annotations.OnDelete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import com.maxnet.cursomc.domain.Categoria;
 import com.maxnet.cursomc.repositories.CategoriaRepository;
+import com.maxnet.cursomc.services.exceptions.DataIntegrityException;
 import com.maxnet.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -27,5 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
 	}
 }
