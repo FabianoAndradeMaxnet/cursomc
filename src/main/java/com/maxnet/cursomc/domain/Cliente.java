94b5cbaa8a8ff5b6bba40b6010a600a393419a5e
FabianoAndradeMaxnet <fabianoandrade@terra.com.br>
2020-12-22 21:01:01 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/maxnet/cursomc/domain/Cliente.java
+++ src/main/java/com/maxnet/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -39,8 +40,7 @@ public class Cliente implements Serializable{
 		this.pedidos = pedidos;
 	}
 
-	//@JsonIgnore
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade= CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
